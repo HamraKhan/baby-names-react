4eeca0c94b8547bbec7e3f97e204c199ef73b9d9
Hamra <hamra.khan.b@gmail.com>
2022-04-17 20:50:14 +0300

    Changed color according to gender - Created genderColor() function to change color according to gender of each name -  Added css styling

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import './App.css';
 import Names from './Names.js';
 import babyNamesData from "./babyNamesData.json"; 
 
+function genderColor(gender) {
+  if (gender === "m") {
+    return 'male-color';
+  } 
+  
+  return 'female-color';
+}
+
 function App() {
   return (
     <div className="App">
       {babyNamesData.map((babyName) => {
         return (
-          <div className='baby-name'>
+          <div className={`baby-name ${genderColor(babyName.sex)}`}>
             <Names name={babyName.name} />
           </div>
         );
