7df4961afe708e9e93ebeb83670fd0a9b8b6ef88
Hamra <hamra.khan.b@gmail.com>
2022-04-21 20:22:30 +0300

    Filtered names - Added input element and useState to filter baby names

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 // import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 import Names from './Names.js';
 import babyNamesData from "./babyNamesData.json"; 
+import FilterNames from './FilterNames';
 
 function genderColor(gender) {
   if (gender === "m") {
@@ -12,9 +14,25 @@ function genderColor(gender) {
 }
 
 function App() {
+  const [filter, setFilter] = useState("");
+
   return (
     <div className="App">
+      <div>
+        <input
+          type="text"
+          value={filter}
+          onChange={(event) => setFilter(event.target.value)}
+        />
+      </div>
+
       {babyNamesData
+        .filter((babyName) => {
+          return (
+            babyName.name.toLowerCase().includes(filter.toLowerCase()) ||
+            filter === ""
+          );
+        })
         .sort((a, b) => a.name.localeCompare(b.name))
         .map((babyName) => {
           return (
